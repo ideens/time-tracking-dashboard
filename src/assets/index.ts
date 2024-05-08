1e19b7816a7c10bacb59e23c9269f2e59f8360f3
Ideen Sanei <ideenssanei@gmail.com>
2024-05-08 18:40:10 +0100

    global css and asset export

---
+++ src/assets/index.ts
@@ -0,0 +1,15 @@
+import workIcon from "./icon-work.svg";
+import exerciseIcon from "./icon-exercise.svg";
+import playIcon from "./icon-play.svg";
+import selfCareIcon from "./icon-self-care.svg";
+import studyIcon from "./icon-study.svg";
+import socialIcon from "./icon-social.svg";
+
+export {
+  workIcon,
+  exerciseIcon,
+  playIcon,
+  selfCareIcon,
+  studyIcon,
+  socialIcon,
+};
